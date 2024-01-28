77490f4ce071122036e04c8552418cea1490b166
ar1622 <ar1622@live.mdx.ac.uk>
2024-01-28 16:54:09 +0600

    fff

--- dist/constants/searchField.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.searchField = void 0;
-exports.searchField = [
-    'topic',
-    'location',
-    'price',
-    'space',
-];
